4dad9a5a5c22a83969524d98d34c644c302d1fcb
David Caicedo <davidfcc7@gmail.com>
2021-02-25 15:52:33 -0500

    creacion de seccion proyecto wordpress y creacion del html wordpress

---
+++ js/code.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("botonSaberMasWordpress");
+btn.addEventListener("click", eventoClickWordpress);
+
+function eventoClickWordpress ()
+{
+    location.href = "https://davidfcc7.github.io/wordpress.html";
+}
+
