222380ec72fd825756e8ef485050913176c2b0e4
David Caicedo <davidfcc7@gmail.com>
2021-02-25 16:03:50 -0500

    creacion de html debian y ajuste de seccion debian en proyecto

--- js/code.js
+++ js/code.js
@@ -1,8 +1,16 @@
 var btn = document.getElementById("botonSaberMasWordpress");
 btn.addEventListener("click", eventoClickWordpress);
 
+var btn = document.getElementById("botonSaberMasDebian");
+btn.addEventListener("click", eventoClickDebian);
+
 function eventoClickWordpress ()
 {
     location.href = "https://davidfcc7.github.io/wordpress.html";
+}
+
+function eventoClickDebian ()
+{
+    location.href = "https://davidfcc7.github.io/debian.html";
 }
 
