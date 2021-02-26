c8630ee2eeb16296e88e6728a7edd3897a60fc1f
David Caicedo <davidfcc7@gmail.com>
2021-02-25 22:32:58 -0500

    creacion de html ia, debian y marketing, asignacion de imagenes a las secciones faltantes de proyecto e implementacion de botones en el js con el evento para redireccion

--- js/code.js
+++ js/code.js
@@ -4,6 +4,12 @@ btn.addEventListener("click", eventoClickWordpress);
 var btn = document.getElementById("botonSaberMasDebian");
 btn.addEventListener("click", eventoClickDebian);
 
+var btn = document.getElementById("botonSaberMasMarketing");
+btn.addEventListener("click", eventoClickMarketing);
+
+var btn = document.getElementById("botonSaberMasIa");
+btn.addEventListener("click", eventoClickIa);
+
 function eventoClickWordpress ()
 {
     location.href = "https://davidfcc7.github.io/wordpress.html";
@@ -12,5 +18,15 @@ function eventoClickWordpress ()
 function eventoClickDebian ()
 {
     location.href = "https://davidfcc7.github.io/debian.html";
+}
+
+function eventoClickMarketing ()
+{
+    location.href = "https://davidfcc7.github.io/marketing.html";
+}
+
+function eventoClickIa ()
+{
+    location.href = "https://davidfcc7.github.io/ia.html";
 }
 
