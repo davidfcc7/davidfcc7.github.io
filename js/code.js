22a0e0b1b3246431bf30f11142d2a8c20eed3a11
David Caicedo <davidfcc7@gmail.com>
2021-03-25 09:06:40 -0500

    agregué proyectos dendro de la carpeta de front end

--- js/code.js
+++ js/code.js
@@ -1,32 +1,66 @@
 var btn = document.getElementById("botonSaberMasWordpress");
 btn.addEventListener("click", eventoClickWordpress);
 
-var btn = document.getElementById("botonSaberMasDebian");
-btn.addEventListener("click", eventoClickDebian);
+var btn = document.getElementById("botonDiseño");
+btn.addEventListener("click", eventoClickDiseño);
 
-var btn = document.getElementById("botonSaberMasMarketing");
-btn.addEventListener("click", eventoClickMarketing);
+var btn = document.getElementById("botonTiempoLibre");
+btn.addEventListener("click", eventoClickTiempoLibre);
 
 var btn = document.getElementById("botonSaberMasIa");
 btn.addEventListener("click", eventoClickIa);
+
+var btn = document.getElementById("botonFrontend");
+btn.addEventListener("click", eventoClickFrontend);
+
+var btn = document.getElementById("botonJavaScript");
+btn.addEventListener("click", eventoClickJavaScript);
+
+var btn = document.getElementById("botonMovilBitcoin");
+btn.addEventListener("click", eventoClickMovilBitcoin);
+
+var btn = document.getElementById("botonClonGoogle");
+btn.addEventListener("click", eventoClickClonGoogle);
 
 function eventoClickWordpress ()
 {
     location.href = "https://davidfcc7.github.io/wordpress.html";
 }
 
-function eventoClickDebian ()
+function eventoClickDiseño ()
 {
-    location.href = "https://davidfcc7.github.io/debian.html";
+    location.href = "https://davidfcc7.github.io/creatividad.html";
 }
 
-function eventoClickMarketing ()
+function eventoClickTiempoLibre ()
 {
-    location.href = "https://davidfcc7.github.io/marketing.html";
+    location.href = "https://davidfcc7.github.io/tiempolibre.html";
 }
 
 function eventoClickIa ()
 {
     location.href = "https://davidfcc7.github.io/ia.html";
 }
+
+function eventoClickFrontend ()
+{
+    location.href = "https://davidfcc7.github.io/frontend.html";
+}
+
+function eventoClickJavaScript ()
+{
+    location.href = "https://davidfcc7.github.io/javascript.html";
+}
+
+function eventoClickMovilBitcoin ()
+{
+    location.href = "https://davidfcc7.github.io/escuela-frontend/mobileFirst/index.html";
+}
+
+function eventoClickClonGoogle ()
+{
+    location.href = "https://davidfcc7.github.io/escuela-frontend/retogoogle/index.html";
+}
+
+
 
