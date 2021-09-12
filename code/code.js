2b673e5914af8f64afe481ad796752ed1355a068
David Caicedo <davidfcc7@gmail.com>
2021-09-11 23:11:54 -0500

    nuevo repositorio

---
+++ code/code.js
@@ -0,0 +1,7 @@
+const typed = new Typed ('.typed',{
+    strings: ['Desarrollo Frontend', 'Diseño Web', 'Diseño UX / UI', 'Diseño Wordpress'],
+    typeSpeed: 75,
+    startDelay: 300,
+    loop: true,
+    backSpeed: 55
+})
