83f85ea8307d01b025119e343c7c83e02853bcc7
David Caicedo <davidfcc7@gmail.com>
2021-10-09 19:46:07 -0500

    modificacion del menu de navegacion, los card fueron excluidos para comenzar a llenalos uno a uno y el fondo del perfil se cambió

--- code/code.js
+++ code/code.js
@@ -1,5 +1,5 @@
 const typed = new Typed ('.typed',{
-    strings: ['Desarrollo Frontend', 'Diseño Web', 'Diseño UX / UI', 'Diseño Wordpress'],
+    strings: ['Desarrollo Web/Movil', 'Diseño Web', 'Diseño Grafico', 'wordpress', 'Videojuegos'],
     typeSpeed: 75,
     startDelay: 300,
     loop: true,
