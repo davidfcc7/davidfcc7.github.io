909f6afd5a89f83f40fa4b66481dc9d3902d6cac
David Caicedo <davidfcc7@gmail.com>
2021-12-16 19:50:31 -0500

    finalizacion del programa

--- proyectos/DesarrolloWeb/pesoEnPlanetas/codigo/main.js
+++ proyectos/DesarrolloWeb/pesoEnPlanetas/codigo/main.js
@@ -1,16 +1,28 @@
-/* VARIABLES DE ENTRADA */
-let gravedadTierra = 9.8;
-let gravedadMarte = 3.7;
-let gravedadJupiter = 24.8;
-let peso = 94;
-
-/* VARIABLES DE SALIDA */
-
-let pesoMarte;
-let pesoJupiter;
 
 
-pesoMarte = (peso * gravedadMarte) / gravedadTierra;
-pesoJupiter = (peso * gravedadJupiter) / gravedadTierra;
+    /* VARIABLES DE ENTRADA */
+    let gravedadTierra = 9.8;
+    let gravedadMarte = 3.7;
+    let gravedadJupiter = 24.8;
+    let peso = parseInt(prompt("¿Cuanto pesas en Kilogramos?"))
+    let planeta = parseInt(prompt("¿Que planeta deseas elegir: 1. Marte o 2. Jupiter"))
 
-document.write("Mi peso en Marte es: <strong>" + pesoMarte.toFixed(2) + "kg </strong> y mi peso en Jupiter es: <strong>" + pesoJupiter.toFixed(2) + "kg </strong>");
+    /* VARIABLES DE SALIDA */
+
+    let pesoMarte;
+    let pesoJupiter;
+
+    
+
+    if (planeta === 1) {
+        pesoMarte = (peso * gravedadMarte) / gravedadTierra;
+        alert("Mi peso en Marte es: <strong>" + pesoMarte.toFixed(2) + "kg</strong><br>")
+    }
+    if (planeta === 2) {
+        pesoJupiter = (peso * gravedadJupiter) / gravedadTierra;
+        alert("Mi peso en Jupiter es: <strong>" + pesoJupiter.toFixed(2) + "kg</strong>")
+    }
+    if (planeta != 1 && planeta != 2) {
+        alert("ESTA OPCION NO EXISTE!!!!")
+    }
+
