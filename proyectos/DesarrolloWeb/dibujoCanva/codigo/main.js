eb454b8d728c9ea58dbd357ac06536d32094caee
David Caicedo <davidfcc7@gmail.com>
2021-12-21 19:52:21 -0500

    implementacion de funcion en el codigo de lineas de colores

--- proyectos/DesarrolloWeb/dibujoCanva/codigo/main.js
+++ proyectos/DesarrolloWeb/dibujoCanva/codigo/main.js
@@ -1,39 +1,20 @@
 var pantalla = document.getElementById("dibujoCanvas");
 var lienzo = pantalla.getContext("2d");
-
-/* FUNCIONES PARA DIBUJAR SOBRE CANVAS */
-
-lienzo.beginPath();
-
-lienzo.strokeStyle = "blue";
-/* TECHO IZQUIERDO */
-lienzo.moveTo(150,10); /* X - Y */
-lienzo.lineTo(50,50);
-/* TECHO DERECHO */
-lienzo.moveTo(150,10); /* X - Y */
-lienzo.lineTo(250,50);
-/* BASE TECHO */
-lienzo.moveTo(50,50); /* X - Y */
-lienzo.lineTo(250,50);
-/* PARED IZQUIERDA */
-lienzo.moveTo(50,50); /* X - Y */
-lienzo.lineTo(50,110);
-/* PARED DERECHA */
-lienzo.moveTo(250,50); /* X - Y */
-lienzo.lineTo(250,110);
-/* BASE PAREDES */
-lienzo.moveTo(250,110); /* X - Y */
-lienzo.lineTo(50,110);
-/* PUERTA */
-lienzo.moveTo(200,110);
-lienzo.lineTo(200,80);
-lienzo.moveTo(100,110);
-lienzo.lineTo(100,80);
-lienzo.moveTo(200,80);
-lienzo.lineTo(100,80);
-lienzo.moveTo(150,110);
-lienzo.lineTo(150,80);
-
-
-lienzo.stroke();
-lienzo.closePath();
+dibujarCasa("blue",150,10,50,50)/* TECHO IZQUIERDO */
+dibujarCasa("black",150,10,250,50)/* TECHO DERECHO */
+dibujarCasa("orange",50,50,250,50)/* BASE TECHO */
+dibujarCasa("gray",50,50,50,110)/* PARED IZQUIERDA */
+dibujarCasa("green",250,50,250,110)/* PARED DERECHA */
+dibujarCasa("yellow",250,110,50,110)/* BASE PAREDES */
+dibujarCasa("brown",200,110,200,80)/* PUERTA */
+dibujarCasa("red",100,110,100,80)
+dibujarCasa("violet",200,80,100,80)
+dibujarCasa("purple",150,110,150,80)
+function dibujarCasa(color, xinicial, yinicial, xfinal, yfinal){
+    lienzo.beginPath()
+    lienzo.strokeStyle = color
+    lienzo.moveTo(xinicial, yinicial)
+    lienzo.lineTo(xfinal, yfinal)
+    lienzo.stroke()
+    lienzo.closePath()
+}
