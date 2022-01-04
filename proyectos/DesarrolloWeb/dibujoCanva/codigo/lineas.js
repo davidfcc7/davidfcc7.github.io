9e4ed58cfb37725430a15eb95a4f4e6e3b8b76e9
David Caicedo <davidfcc7@gmail.com>
2022-01-04 12:32:56 -0500

    card portafolio con proyecto 3dswipe

--- proyectos/DesarrolloWeb/dibujoCanva/codigo/lineas.js
+++ proyectos/DesarrolloWeb/dibujoCanva/codigo/lineas.js
@@ -1,23 +1,38 @@
-var pantalla = document.getElementById("dibujoCanvas");
-var lienzo = pantalla.getContext("2d");
-var lineas = 30;
-var l = 0; 
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
 
-var color = "blue";
-var yini, xfin;
+var d = document.getElementById("dibujito");
+var ancho = d.width;
+var lienzo = d.getContext("2d");
 
-while(l < lineas){
-    yini = 10 * l
-    xfin = 10 * (l+1)
-    dibujarLineas (color, 0, yini, xfin, 300)
-    l = l + 1
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
 }
 
-function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal){
-    lienzo.beginPath()
-    lienzo.strokeStyle = color
-    lienzo.moveTo(xinicial, yinicial)
-    lienzo.lineTo(xfinal, yfinal)
-    lienzo.stroke()
-    lienzo.closePath()
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#FAA";
+  var espacio = ancho / lineas;
+
+  for(l = 0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+  }
+
+  dibujarLinea(colorcito, 1,1,1,299);
+  dibujarLinea(colorcito, 1,299,299,299);
 }
+
