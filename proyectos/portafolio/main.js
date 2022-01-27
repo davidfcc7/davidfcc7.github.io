a7dd6e335a8e6520f3b7d901a26ae719e06c99fa
David Caicedo <davidfcc7@gmail.com>
2022-01-26 22:56:13 -0500

    perfil portafolio

---
+++ proyectos/portafolio/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed', {
+    strings: ['Html5', 'Css', 'Sass', 'JavaScript', 'Figma', 'UX/UI'],
+    typeSpeed: 75,
+    startDelay: 300,
+    loop: true,
+    backSpeed: 55
+  });
+
