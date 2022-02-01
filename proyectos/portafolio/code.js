757288f993e5d7cb23d5e3a2085d11fc180acf1e
David Caicedo <davidfcc7@gmail.com>
2022-01-31 21:41:17 -0500

    portafolio imagenes maquetacion

---
+++ proyectos/portafolio/code.js
@@ -0,0 +1,7 @@
+const typed = new Typed ('.typed',{
+    strings: ['Desarrollo Web/Movil', 'Diseño Web', 'Diseño Grafico', 'wordpress', 'Videojuegos'],
+    typeSpeed: 75,
+    startDelay: 300,
+    loop: true,
+    backSpeed: 55
+})
