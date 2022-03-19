16f87c580159993b8acd1d7704c812e199a78b21
David Caicedo <davidfcc7@gmail.com>
2022-03-19 10:26:24 -0500

    reestructuracion del portafolio

--- code/code.js
+++
@@ -1,7 +0,0 @@
-const typed = new Typed ('.typed',{
-    strings: ['Figma', 'Html5', 'Css3', 'Sass', 'Bootstrap', 'JavaScript', 'Wordpress'],
-    typeSpeed: 75,
-    startDelay: 300,
-    loop: true,
-    backSpeed: 55
-})
