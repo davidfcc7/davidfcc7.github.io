6c3c6d74e840abbdb86c304302755e1cb49b917d
David Caicedo <davidfcc7@gmail.com>
2022-05-08 15:13:39 -0500

    agregar productos individuales en el carrito

--- Proyectos/HojaVerde/Assets/js/menu-movil.js
+++ Proyectos/HojaVerde/Assets/js/menu-movil.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
     $('.diseno-menu').click(() => {
         $('.list-menu').toggleClass('active');
     })
+
+    $('.icono-home-cart').click(() => {
+        $('.cart-products').toggleClass('active');
+    })
 })
