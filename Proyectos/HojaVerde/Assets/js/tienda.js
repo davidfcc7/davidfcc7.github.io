e51dfce4f4a12c06e29444e03df5aa21885f489b
David Caicedo <davidfcc7@gmail.com>
2022-05-22 21:43:28 -0500

    pedir productos por whatsapp

--- Proyectos/HojaVerde/Assets/js/tienda.js
+++ Proyectos/HojaVerde/Assets/js/tienda.js
@@ -111,7 +111,7 @@ function updateShoppingCartTotal() {
   }
 
 function botonComprarClicked () {
-    const productWhatsapp = 'https://wa.me/3058804870/?text=' + shoppingCartTotal.textContent;
+    const productWhatsapp = 'https://wa.me/3058804870/?text=' + containerTableDetailsProducts.textContent;
     console.table(productWhatsapp);
     window.location.href = productWhatsapp
     /* containerTableDetailsProducts.innerHTML = '';
