3d164e40177e359096acbcefd6d44d2b467a5bfa
David Caicedo <davidfcc7@gmail.com>
2022-05-25 20:36:33 -0500

    zoom imagenes

--- Proyectos/HojaVerde/Assets/js/zoom.js
+++ Proyectos/HojaVerde/Assets/js/zoom.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $('.Pimenton').click(function(){
-        $('.Pimenton').toggleClass('zoom');
-    })
+    $( ".pimenton" ).click(function() {
+        $( ".pimenton" ).each(function() {
+          $( this ).toggleClass( "zoom" );
+        });
+    });
+
+    $( ".Zucchini" ).click(function() {
+        $( ".Zucchini" ).each(function() {
+          $( this ).toggleClass( "zoom-left" );
+        });
+    });
 })
