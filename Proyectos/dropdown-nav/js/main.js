b539cf1f28b676fff618840d4e7e85e5e836ded2
David Caicedo <davidfcc7@gmail.com>
2022-09-30 21:44:01 -0500

    ajustando menu movil

--- Proyectos/dropdown-nav/js/main.js
+++ Proyectos/dropdown-nav/js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
     $('.menu-hamburguer').click(function(){
         $(this).toggleClass('active');
-				$('.container--menu-nav').toggleClass('active');
+			$('.container--menu-nav').toggleClass('active');
     })
 
-		$('li.icon').click(function(){
-			$(this).toggleClass('active');
+	/* $('li.icon').click(function(){
+		$(this).toggleClass('active');
+		$('.container-submenu').toggleClass('active');
+	}) */
+
+	$('li.icon').click(function(){
+		$(this).toggleClass(function(index){
+			return "active" + (index+1);
+		});
 	})
 });
