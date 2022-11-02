39c3d00a46f3c207c7f40d9a09b2185639c2dc1a
David Caicedo <davidfcc7@gmail.com>
2022-11-01 20:27:47 -0500

    carpeta con ejercicios de javascript

---
+++ Proyectos/ejercicios-javascript/piedra-papel-tijera.js
@@ -0,0 +1,39 @@
+var piedra=0,papel=1,tijera=2;
+var min=0,max=2;
+var random=Math.floor(Math.random()*(max-min+1)+min);
+
+/* function juego (usuario, maquina){
+    if ((usuario === 0)&&(maquina === 2)||(usuario === 1)&&(maquina === 0)||(usuario === 2)&&(maquina === 1)){
+        console.log("gana usuario");
+    }
+    else if ((usuario === maquina)){
+        console.log("Empate");
+    }
+    else{
+        console.log("gana maquina");
+    }
+}
+juego(1,random); */
+
+function juegoSwitch (usuario, maquina){
+    //con true los casos van a suceder
+    switch (true){
+        case (usuario === 0)&&(maquina === 2):
+            console.log("Usuario Gana");
+            break;
+        case (usuario === 1)&&(maquina === 0):
+            console.log("Usuario Gana");
+            break;
+        case (usuario === 2)&&(maquina === 1):
+            console.log("Usuario Gana");
+            break;
+        case (usuario === maquina):
+            console.log("Empate");
+            break;
+            default:
+                console.log("Gana Maquina");
+    }
+}
+
+juegoSwitch(1,random);
+
