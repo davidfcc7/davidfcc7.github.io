9d559d3fc15aadddd603d51aae93182e3b127441
David Caicedo <davidfcc7@gmail.com>
2022-12-31 18:22:56 -0500

    funcionalidad de un slider en javascript con prev y next

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -13,3 +13,41 @@ tabs__button.forEach(button=>{
         })
     })
 })
+
+/* funcionalidad del slider con vanilla javascript */
+
+const card__carousel=document.getElementById('card__carousel')
+const prev__button=document.getElementById('prev__button')
+const next__button=document.getElementById('next__button')
+
+let valorInicial=0;
+
+prev__button.addEventListener('click',()=>{
+    valorInicial--
+    if(valorInicial<0){
+        valorInicial=card__carousel.children.length-1;
+    }
+    updateCarousel()
+})
+
+next__button.addEventListener('click',()=>{
+    valorInicial++
+    if(valorInicial>=card__carousel.children.length){
+        valorInicial=0
+    }
+    updateCarousel()
+})
+
+function updateCarousel(){
+    for(let i=0;i<card__carousel.children.length;i++){
+        const card=card__carousel.children[i];
+        if(i===valorInicial){
+            card.style.display='block';
+        }
+        if(i===valorInicial){
+            card.style.display='none';
+        }
+    }
+}
+
+updateCarousel()
