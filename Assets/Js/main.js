da30b81a9fec552b3e21b3a3e6fcff65a7ff5629
David <davidfcc7@gmail.com>
2023-04-25 21:47:00 -0500

    refactorizacion del codigo del portafolio

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -1,15 +1,15 @@
 /* funcionalidad de tabs con vanilla javascript */
 
-const tabs__button=document.querySelectorAll('.tabs__button')
-const tabs__content=document.querySelectorAll('.tabs__content')
+const tabs__button=document.querySelectorAll('.tabs__button');
+const tabs__content=document.querySelectorAll('.tabs__content');
 
 tabs__button.forEach(button=>{
     button.addEventListener('click',()=>{
         const tabContentId=button.dataset.tabContent
         const tabContent=document.getElementById(tabContentId)
         tabs__content.forEach(content=>{
-            content.classList.remove('active')
-            tabContent.classList.add('active')
+            content.style.display = "none";
+            tabContent.style.display = "block";
         })
     })
 })
@@ -52,7 +52,7 @@ function updateCarousel(){
 
 updateCarousel()
 
-/* funcionalidad acordios de experiencia */
+/* funcionalidad acordion de experiencia */
 
 var titleExperience = document.querySelectorAll('.title-experience'); 
 var contentExperience = document.querySelectorAll('.content-experience'); 
