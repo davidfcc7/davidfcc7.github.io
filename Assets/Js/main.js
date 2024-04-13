cc1bb6dbf69acf4dbd26bb19382909566dcb7dff
David <davidfcc7@gmail.com>
2024-04-12 19:10:59 -0500

    Portafolio

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo!")
+const allLTab = document.querySelectorAll(".tab");
+const allContent = document.querySelectorAll(".tab-content");
+
+allLTab.forEach((tab, index) => {
+  tab.addEventListener("click", function () {
+    // Borrar el tab active de todos los contenidos
+    allContent.forEach((content) => {
+      content.classList.remove("tab-active");
+    });
+    allContent[index].classList.add("tab-active");
+  });
+});
